3125669def774d3579f0ecd6a7e654d4ac4d1974
MREZA <rezaworku31@gmail.com>
2026-03-05 21:53:46 +0700

    refactor : hospital specialist and doctors migrate

--- betterhospital-backend/database/migrations/2026_03_05_213616_create_doctors_table.php
+++ betterhospital-backend/database/migrations/2026_03_05_213616_create_doctors_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->index();
+            $table->string('photo');
+            $table->text('about');
+            $table->integer('yoe');
+            $table->foreignId('specialist_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('hospital_id')->constrained()->cascadeOnDelete();
+            $table->string('gender');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- betterhospital-backend/database/migrations/2026_03_05_214808_create_hospital_specialists_table.php
+++ betterhospital-backend/database/migrations/2026_03_05_214808_create_hospital_specialists_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('hospital_specialists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('hospital_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('specialist_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
+            $table->softDeletes();
+            $table->unique(['hospital_id','specialist_id']);
         });
     }
 
