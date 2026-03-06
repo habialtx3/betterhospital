048e9a2114f9cfe5a2f45109c1a597ab6dd52900
MREZA <rezaworku31@gmail.com>
2026-03-06 11:52:42 +0700

    choore booking transaction model

--- betterhospital-backend/app/Models/BookingTransaction.php
+++ betterhospital-backend/app/Models/BookingTransaction.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class BookingTransaction extends Model
 {
     //
+    protected $fillable = [
+        'user_id',
+        'doctor_id',
+        'status',
+        'started_at',
+        'time_at',
+        'sub_total',
+        'tax_total',
+        'grand_total',
+        'proof',
+    ];
 }
