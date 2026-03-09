84664ec2dc9cd7f8a406e6c36579fe05e18d5ee3
MREZA <rezaworku31@gmail.com>
2026-03-10 01:17:48 +0700

    chore : hospitalspecialist repository and doctor service

--- betterhospital-backend/app/Repositories/HospitalSpecialistRepository.php
+++ betterhospital-backend/app/Repositories/HospitalSpecialistRepository.php
@@ -1,9 +1,15 @@
 <?php
+
 namespace App\Repositories;
+
+use App\Models\HospitalSpecialist;
 
 class HospitalSpecialistRepository
 {
-    
+    public function existForHospitalAndSpecialist(int $hospitalId, int $specialistId)
+    {
+        return HospitalSpecialist::where('hospital_id', $hospitalId)
+            ->where('specialist_id', $specialistId)
+            ->exists();
+    }
 }
-
-?>
