4fc9a4f77b0d352086e476936d50b8a59903b412
MREZA <rezaworku31@gmail.com>
2026-03-09 16:59:15 +0700

    refactor : hospital repository created

--- betterhospital-backend/app/Repositories/HospitalRepository.php
+++ betterhospital-backend/app/Repositories/HospitalRepository.php
@@ -1,9 +1,32 @@
 <?php
 namespace App\Repositories;
 
+use App\Models\Hospital;
+
 class HospitalRepository
 {
-    
+    public function getAll(array $fields){
+        return Hospital::select($fields)->latest()->paginate('10');
+    }
+
+    public function getById(int $id,array $fields){
+        return Hospital::select($fields)->with(['doctors.specialist','specialists'])->findOrFail($id);
+    }
+
+    public function create($data){
+        return Hospital::create($data);
+    }
+
+    public function edit(int $id, array $data){
+        $hospital = Hospital::findOrFail($id);
+        $hospital->update($data);
+        return $hospital;
+    }
+
+    public function delete(int $id){
+        $hospital = Hospital::findOrFail($id);
+        $hospital->delete();
+    }
 }
 
 ?>
