43349dd5811000d7736d0c7996d3f5216d22c6a2
MREZA <rezaworku31@gmail.com>
2026-03-13 23:53:10 +0700

    refactor : adding api basepath route

--- betterhospital-backend/routes/api.php
+++ betterhospital-backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\HospitalController;
 use App\Http\Controllers\SpecialistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,8 +11,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('specialists',SpecialistController::class);
-Route::apiResource('doctors',SpecialistController::class);
-Route::apiResource('hospitals',SpecialistController::class);
+Route::apiResource('doctors',DoctorController::class);
+Route::apiResource('hospitals',HospitalController::class);
 
 
 
