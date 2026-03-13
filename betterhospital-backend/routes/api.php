e35c3168f22c6cf9d5acf92928f7f0f588b6c482
MREZA <rezaworku31@gmail.com>
2026-03-13 23:49:17 +0700

    chore :new  api route

--- betterhospital-backend/routes/api.php
+++ betterhospital-backend/routes/api.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\SpecialistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('specialists',SpecialistController::class);
+Route::apiResource('doctors',SpecialistController::class);
+Route::apiResource('hospitals',SpecialistController::class);
+
 
 
