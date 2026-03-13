f125f579a09fbe1e06e9e1f2b0a88b5cfb8fb61a
MREZA <rezaworku31@gmail.com>
2026-03-14 00:07:14 +0700

    chore : initialize route for doctor, transaction et

--- betterhospital-backend/routes/api.php
+++ betterhospital-backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BookingTransactionController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\HospitalSpecialistController;
+use App\Http\Controllers\MyOrderController;
 use App\Http\Controllers\SpecialistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,5 +17,15 @@ Route::apiResource('specialists',SpecialistController::class);
 Route::apiResource('doctors',DoctorController::class);
 Route::apiResource('hospitals',HospitalController::class);
 
+Route::post('hospitals/{hospital}/specialists',[HospitalSpecialistController::class,'attach']);
+Route::delete('hospitals/{hospital}/specialists/{specialist}',[HospitalSpecialistController::class,'detach']);
 
+Route::apiResource('transactions',BookingTransactionController::class);
+Route::patch("/transactions/{id}/status",[BookingTransactionController::class, 'updateStatus']);
 
+Route::get('/doctor-filter',[DoctorController::class, 'filterBySpecialistAndHospital']);
+Route::get('/doctors/{doctorId}/available-slots',[DoctorController::class,'availableSlots']);
+
+Route::get('my-orders',[MyOrderController::class,'index']);
+Route::post('my-orders',[MyOrderController::class,'store']);
+Route::get('my-orders/{id}',[MyOrderController::class,'show']);
