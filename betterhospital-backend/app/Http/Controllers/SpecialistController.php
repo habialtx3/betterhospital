7fa8e2e2b91856d206e3d6e44938b3d914666f8e
MREZA <rezaworku31@gmail.com>
2026-03-14 16:43:54 +0700

    chore : initialize resource files

--- betterhospital-backend/app/Http/Controllers/SpecialistController.php
+++ betterhospital-backend/app/Http/Controllers/SpecialistController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\SpecialistService;
 use Illuminate\Http\Request;
 
 class SpecialistController extends Controller
 {
-    //
+    private $specialistService;
+
+    public function __construct(SpecialistService $specialistService)
+    {
+        $this->specialistService = $specialistService;
+    }
+
+    public function index()
+    {
+        $fields = ['id','name','photo','price',];
+        $specialist = $this->specialistService->getAll($fields);
+        return response()->json(SpecialistResour)
+    }
 }
